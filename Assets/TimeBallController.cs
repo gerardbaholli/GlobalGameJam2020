10b913bdcf4ba8cd2db5d781d3ec5cced0ae73eb
Massimo Frasson <massimo@belkadigital.com>
2020-02-01 18:10:17 +0100

    Timeballs turnoff after fraction of time

---
+++ Assets/TimeBallController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class TimeBallController : MonoBehaviour
+{
+    [SerializeField] private Sprite enabled;
+    [SerializeField] private Sprite disabled;
+
+    [SerializeField] private Image _image;
+
+    private void Reset()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    public void Enable()
+    {
+        _image.sprite = enabled;
+    }
+
+    public void Disable()
+    {
+        _image.sprite = disabled;
+    }
+}
--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
@@ -9,9 +7,14 @@ public class Timer : MonoBehaviour
     [SerializeField] private int numberOfBalls;
     [SerializeField] private GameObject timeBallPrefab;
 
+    private TimeBallController[] _ballControllers;
+    private float _elapsedTime;
+    private float _timePerBall;
+    private bool _gameOver;
+
     private void OnValidate()
     {
-        if (timeBallPrefab == null)
+        if (timeBallPrefab == null || transform.childCount == numberOfBalls)
             return;
 
         foreach (Transform child in transform)
@@ -27,5 +30,31 @@ public class Timer : MonoBehaviour
         {
             Instantiate(timeBallPrefab, transform);
         }
+    }
+
+    private void Start()
+    {
+        _ballControllers = GetComponentsInChildren<TimeBallController>();
+        _timePerBall = totalTime / numberOfBalls;
+    }
+
+    private void Update()
+    {
+        if(_gameOver)
+            return;
+
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime > _timePerBall)
+        {
+            var index = (int) (_elapsedTime / _timePerBall) - 1;
+            _ballControllers[index].Disable();
+        }
+        
+        if (totalTime <= _elapsedTime)
+        {
+            _gameOver = true;
+            // GameEnd
+        } 
     }
 }
