eb319def5495221344a09f60f63862c7a4db3868
Massimo Frasson <massimo@belkadigital.com>
2020-02-01 17:30:42 +0100

    Generates a given number of timeballs

---
+++ Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float totalTime;
+    [SerializeField] private int numberOfBalls;
+    [SerializeField] private GameObject timeBallPrefab;
+
+    private void OnValidate()
+    {
+        if (timeBallPrefab == null)
+            return;
+
+        foreach (Transform child in transform)
+        {
+            UnityEditor.EditorApplication.delayCall += () =>
+            {
+                if (child.gameObject != null)
+                    DestroyImmediate(child.gameObject);
+            };
+        }
+
+        for (int i = 0; i < numberOfBalls; i++)
+        {
+            Instantiate(timeBallPrefab, transform);
+        }
+    }
+}
