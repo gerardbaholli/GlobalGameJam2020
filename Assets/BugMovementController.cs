4258cdb0fffc9b59adbe500c8e2280820a88f869
Gerard Baholli <gerardbaholli@live.it>
2020-02-02 00:06:28 +0100

    Adds bugs movement

---
+++ Assets/BugMovementController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BugMovementController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField, Range(0f, 3f), Tooltip("How big is the step each frame?")] private float speed = .1f;
+    [SerializeField, Range(0f, 5f), Tooltip("How long does it take to change rotation?")] private float straightLine = 3f;
+    
+    private float _lastRotationUpdate;
+    private float rotation;
+    
+
+    private void Reset()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Time.timeSinceLevelLoad - _lastRotationUpdate > straightLine)
+        {
+            rotation = Random.Range(0f, 360f);
+            _rigidbody2D.SetRotation(rotation);
+            _lastRotationUpdate = Time.timeSinceLevelLoad;
+        }
+
+        Vector2 newPosition = _rigidbody2D.position + (Vector2) transform.up * speed;
+        _rigidbody2D.MovePosition(newPosition);
+    }
+}
