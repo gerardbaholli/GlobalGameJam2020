bb8790c3dcc2c8902791f3598662969bd23ab8d1
Massimo Frasson <massimo@belkadigital.com>
2020-02-01 14:21:53 +0100

    Bootstrap level design

---
+++ Assets/CharacterMovementController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterMovementController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    private void Reset()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+}
