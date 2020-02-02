d778ab5f30e3269054c820a7c7a2f17d1bab7dcd
Massimo Frasson <massimo@belkadigital.com>
2020-02-02 08:23:53 +0100

    Main menu loads first level on play

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelManager : Singleton<LevelManager>
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void LoadLevel(int index)
+    {
+        SceneManager.LoadScene(index.ToString());
+    }
+}
