f80df17f61ea488a149c6fcc0eca4ab3d18cb131
Massimo Frasson <massimo@belkadigital.com>
2020-02-02 07:26:41 +0100

    Spawner refactor and adds bugscontainer

---
+++ Assets/BugContainer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class BugContainer : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject bugPrefab;
+    public UnityEvent OnBugsKilled = new UnityEvent();
+
+    private void Update()
+    {
+        if (transform.childCount <= 0)
+        {
+            OnBugsKilled?.Invoke();
+        }
+    }
+
+    public void InstantiateBug(Vector3 position)
+    {
+        Instantiate(bugPrefab, position, Quaternion.identity, transform);
+    }
+}
--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] private BugContainer _container;
 
-     [SerializeField] private GameObject bugPrefab;
-     
-     private void Awake()
-     {
-          foreach (Transform childTransform in transform)
-          {
-               Instantiate(bugPrefab, childTransform.position, Quaternion.identity);
-          }
-     }
+    private void Awake()
+    {
+        foreach (Transform childTransform in transform)
+        {
+            _container.InstantiateBug(childTransform.position);
+        }
+    }
 }
