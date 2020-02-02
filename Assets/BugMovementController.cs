828ea1a0b45ab1d5f73fbf186a760dce0ade2879
Massimo Frasson <massimo@belkadigital.com>
2020-02-02 07:17:48 +0100

    Basic bug kill behaviour

--- Assets/BugMovementController.cs
+++ Assets/BugMovementController.cs
@@ -5,9 +5,13 @@ using Random = UnityEngine.Random;
 public class BugMovementController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody2D;
-    [SerializeField, Range(0f, 3f), Tooltip("How big is the step each frame?")] private float speed = .1f;
-    [SerializeField, Range(0f, 5f), Tooltip("How long does it take to change rotation?")] private float straightLine = 3f;
-    
+
+    [SerializeField, Range(0f, 3f), Tooltip("How big is the step each frame?")]
+    private float speed = .1f;
+
+    [SerializeField, Range(0f, 5f), Tooltip("How long does it take to change rotation?")]
+    private float straightLine = 3f;
+
     private float _lastRotationUpdate;
     private float rotation;
     private Camera _camera;
---
+++ Assets/KillController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class KillController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+
+    private Vector2 _boxSize;
+
+    private void Reset()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+        _boxSize = new Vector2(1f, 2f) * gameObject.transform.localScale;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            RaycastHit2D hit = Physics2D.BoxCast(_rigidbody2D.position, _boxSize, 0, Vector2.zero, 0f,
+                1 << LayerMask.NameToLayer("Bug"));
+            if (hit.collider != null)
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
